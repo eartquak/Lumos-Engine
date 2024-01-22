1ab78145ebd74701a71d2128aef4eddc54a04582
Saphereye <adarshdas950@gmail.com>
2024-01-22 10:46:01 +0530

    chore: fixed `texture.cpp`

--- examples/texture.cpp
+++ examples/texture.cpp
@@ -2,34 +2,26 @@
 #include <vector>
 
 #include "lumos.h"
-#include "textures.h"
 #include "shader.h"
+#include "textures.h"
 
-void glide(rect& texRect) {
-    texRect.pos.x += 0.1f;
-}
+void glide(rect& texRect) { texRect.pos.x += 0.1f; }
 
 int main() {
     App app = App(800, 800, "Testing Window", false, true);
-    printf("setting up rects\n");
+    spdlog::debug("Setting up rects");
     rect texRect;
     texRect.pos = {-0.5f, -0.5f};
-    texRect.dim = { 1.0f,  1.0f};
+    texRect.dim = {1.0f, 1.0f};
     Texture::initShader();
-    Texture *tex = new Texture("../assets/ass.png", GL_RGB, GL_UNSIGNED_BYTE, texRect, 0);
+    Texture* tex =
+        new Texture("../assets/ass.png", GL_RGB, GL_UNSIGNED_BYTE, texRect, 0);
 
-
-    app.add_system(SystemType::Update, [tex]() {
-            tex->draw();
-        })
-        .add_system(SystemType::Update, [&texRect, tex]() {
-            texRect.pos.x += 0.01f;  
+    app.add_update_system([tex]() { tex->draw(); })
+        .add_update_system([&texRect, tex]() {
+            texRect.pos.x += 0.01f;
             tex->updateRect(texRect);
         });
 
     app.run();
 }
-
-
-    
-
